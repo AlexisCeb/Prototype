f921fa09fe140d4c83e0fa9244e16d6ac035e423
AlexisCeb <18308051220243@cbtis122.edu.mx>
2021-11-17 21:39:30 -0700

    Inicio de la seccion experimental

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Ver 1.0</title>
+        <title>Experimental</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
