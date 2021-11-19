01e805879c31c352ecd7f5fd1d032a5b35a77e01
AlexisCeb <18308051220243@cbtis122.edu.mx>
2021-11-18 19:29:27 -0700

    Prueba de video

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <html lang="ES">
     <head>
-        <title>Chinito 2</title>
+        <title>Chinito 3</title>
         <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
         <link rel="stylesheet" href="/css/style.css">
     </head>
