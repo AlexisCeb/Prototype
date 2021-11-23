e440bc8c83220ee013b3cc358fa7ac19954b291a
AlexisCeb <18308051220243@cbtis122.edu.mx>
2021-11-23 11:45:48 -0700

    separacion de footer en includes y templates

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,38 @@
+
+<footer class="bg-white h-64">
+    <hr class="container px-6 py-4 mx-auto">
+    <div class="lg:flex">
+        <div class="w-full -mx-6 lg:w-2/5">
+            <div class="px-6">
+                <div>
+                    <p class="text-xl font-bold m-6 text-gray-800">No sé que lleva el footer</p>
+                </div>
+                <p class="max-w-md mt-2 text-gray-500 m-6">No sé, poner alguna información del sitio web tal vez</p>
+            </div>
+        </div>
+        <div class="mt-6 lg:mt-0 lg:flex-1">
+            <div class="grid grid-cols-2 gap-6 md:grid-cols-3">
+                <div>
+                    <h3 class="text-gray-800 uppercase">Tema x</h3>
+                    <p class="text-gray-600">Subtema/link</p>
+                    <p class="text-gray-600">Subtema/link</p>
+                </div>
+
+                <div>
+                    <h3 class="text-gray-800 uppercase">Tema y</h3>
+                    <p class="text-gray-600">Subtema/link</p>
+                    <p class="text-gray-600">Subtema/link</p>
+                </div>
+                <div>
+                    <h3 class="text-gray-800 uppercase">Tema z</h3>
+                    <p class="text-gray-600">Subtema/link</p>
+                    <p class="text-gray-600">Subtema/link</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr class="h-px my-6 mx-7 bg-gray-500 border-none">
+    <div>
+        <p class="text-center text-gray-900">&copy;2021 Idk</p>
+    </div>
+</footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,59 +7,23 @@
     <body>
     <div class="vin">
         <div class="container mx-auto my-10">
-                <div class="w-80 rounded-md shadow-2xl bg-white">
-                    <div class="h-40 imgmon rounded-t-md">
-                        <img src="https://www.atlasandboots.com/wp-content/uploads/2019/05/ama-dablam2-most-beautiful-mountains-in-the-world.jpg" alt="">
-                    </div>
-                    <div class="px-4 py-3 text-gray-600">
-                        <div class="text-lg font-bold">The Coldest Sunset</div>
-                        <div class="text-sm pr-3 py-2">Lorem impsum dolor sit amet, consecteur adipicing elit. Voluptatibus quia, nulla! Maiores et preferendis eaque, exercitationem praesentium nihil</div>
-                    </div>
-                    <div class="text-xs text-gray-600 font-bold pt-2 pb-5 px-3">
-                        <div class="inline bg-gray-200 m-1 rounded-full py-1 px-2">#photography</div>
-                        <div class="inline bg-gray-200 m-1 rounded-full py-1 px-2">#travel</div>
-                        <div class="inline bg-gray-200 m-1 rounded-full py-1 px-2">#winter</div>
-                    </div>
-                </div>
-        </div>
-        <footer class="bg-white h-64">
-        <hr class="container px-6 py-4 mx-auto">
-        <div class="lg:flex">
-        <div class="w-full -mx-6 lg:w-2/5">
-        <div class="px-6">
-            <div>
-                <p class="text-xl font-bold m-6 text-gray-800">No sé que lleva el footer</p>
-        </div>
-                <p class="max-w-md mt-2 text-gray-500 m-6">No sé, poner alguna información del sitio web tal vez</p>
-        </div>
-        </div>
-        <div class="mt-6 lg:mt-0 lg:flex-1">
-        <div class="grid grid-cols-2 gap-6 md:grid-cols-3">
-        <div>
-                 <h3 class="text-gray-800 uppercase">Tema x</h3>
-                 <p class="text-gray-600">Subtema/link</p>
-                 <p class="text-gray-600">Subtema/link</p>
-                 </div>
+{{--                <div class="w-80 rounded-md shadow-2xl bg-white">--}}
+{{--                    <div class="h-40 imgmon rounded-t-md">--}}
+{{--                        <img src="https://www.atlasandboots.com/wp-content/uploads/2019/05/ama-dablam2-most-beautiful-mountains-in-the-world.jpg" alt="">--}}
+{{--                    </div>--}}
+{{--                    <div class="px-4 py-3 text-gray-600">--}}
+{{--                        <div class="text-lg font-bold">The Coldest Sunset</div>--}}
+{{--                        <div class="text-sm pr-3 py-2">Lorem impsum dolor sit amet, consecteur adipicing elit. Voluptatibus quia, nulla! Maiores et preferendis eaque, exercitationem praesentium nihil</div>--}}
+{{--                    </div>--}}
+{{--                    <div class="text-xs text-gray-600 font-bold pt-2 pb-5 px-3">--}}
+{{--                        <div class="inline bg-gray-200 m-1 rounded-full py-1 px-2">#photography</div>--}}
+{{--                        <div class="inline bg-gray-200 m-1 rounded-full py-1 px-2">#travel</div>--}}
+{{--                        <div class="inline bg-gray-200 m-1 rounded-full py-1 px-2">#winter</div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
 
-        <div>
-        <h3 class="text-gray-800 uppercase">Tema y</h3>
-        <p class="text-gray-600">Subtema/link</p>
-        <p class="text-gray-600">Subtema/link</p>
         </div>
-        <div>
-               <h3 class="text-gray-800 uppercase">Tema z</h3>
-               <p class="text-gray-600">Subtema/link</p>
-               <p class="text-gray-600">Subtema/link</p>
-        </div>
-        </div>
-        </div>
-        </div>
-            <hr class="h-px my-6 mx-7 bg-gray-500 border-none">
-        <div>
-        <p class="text-center text-gray-900">&copy;2021 Idk</p>
-                </div>
-            </div>
-        </footer>
     </div>
+    @extends('templates/master')
     </body>
 </html>
