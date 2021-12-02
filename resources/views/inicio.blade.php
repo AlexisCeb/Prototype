dad890e339fa198a91c751730b5a1f5cda52040e
AlexisCeb <18308051220243@cbtis122.edu.mx>
2021-12-02 15:55:40 -0700

    Actualizacion dic 2 21

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,13 @@
+@extends('templates/master')
+@section('contenido')
+    <div class="container mx-auto mt-10">
+        <div class="grid bg-gray-200 rounded-md shadow-inner p-5 md:grid-cols-2 ">
+            <div class="bg-red-300">
+
+            </div>
+            <div class="bg-blue-300">
+
+            </div>
+        </div>
+    </div>
+@stop
